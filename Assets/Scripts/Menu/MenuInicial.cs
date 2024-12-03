f513a844cc247872f2c1aea7d5fd33f8c0cb869f
Alejandro Algarin <alejandro.algarin@hotmail.com>
2024-12-03 11:59:31 -0300

    Update MainCharacte, MenuPausa

--- Assets/Scripts/Menu/MenuInicial.cs
+++ Assets/Scripts/Menu/MenuInicial.cs
@@ -1,3 +1,7 @@
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +9,11 @@ using UnityEngine.SceneManagement;
 
 public class MenuInicial : MonoBehaviour
 {
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
     public void Jugar()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -14,6 +23,9 @@ public class MenuInicial : MonoBehaviour
     {
         Debug.Log("Saliendo del juego...");
         Application.Quit();
+#if UNITY_EDITOR 
+        EditorApplication.ExitPlaymode(); //Solo editor, no funciona en la build!!!
+#endif
     }
 
 }
